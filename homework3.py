edb22072c9a146c1e1d10f71fa9b30ee1a640ead
Danil-Probnik <danilkin290@bk.ru>
2024-07-17 13:26:58 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,6 +2,7 @@ Name='Danil'
 print('Name: '+ Name)
 Age=20
 print('Age:', Age)
-Age='21'
-print('New Age: '+Age)
-
+Age=20
+print('New Age:', Age + 1 )
+Is_student=True
+print('Is student:', Is_student)
